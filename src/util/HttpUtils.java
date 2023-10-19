59592d4a336ebe98b3975117ad87f18c1e0b4809
KoenZhang <2823474269@qq.com>
2023-10-19 22:56:22 +0800

    feat: 新增建立连接和获取文件名字工具类

--- src/util/HttpUtils.java
+++ src/util/HttpUtils.java
@@ -1,16 +1,39 @@
 package util;
 
+import constant.RequestConstant;
+
+import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 
 public class HttpUtils {
+
+
     /**
      * 获取 HttpURLConnection 链接对象
      * @param url
      * @return
      */
-    public static HttpURLConnection getHttpURLConnection(String url) throws MalformedURLException {
+    public static HttpURLConnection getHttpURLConnection(String url) throws IOException {
         URL httpUrl = new URL(url);
+        HttpURLConnection httpURLConnection = (HttpURLConnection) httpUrl.openConnection();
+
+        // 向文件服务器发送标识信息
+        httpURLConnection.setRequestProperty(RequestConstant.USER_AGENT_KEY, RequestConstant.USER_AGENT_VALUE);
+
+
+        return httpURLConnection;
+    }
+
+    /**
+     * 获取下载链接文件名
+     * @param url
+     * @return
+     */
+    public static String getHttpFileName (String url) {
+        int index = url.lastIndexOf(RequestConstant.LEFT_SLASH);
+        return url.substring(index + 1);
     }
 }
